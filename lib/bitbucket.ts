652af2c66f068634b04ba0f608f8f1e02daa4b7d
Ken Howard <ken@simplicatedweb.com>
2017-09-09 05:40:25 -0700

    refactor(Bitbucket): set apiUrl and apiVersion

--- lib/bitbucket.ts
+++ lib/bitbucket.ts
@@ -1,14 +1,20 @@
 export interface BitbucketOptions {
+  apiUrl?: string;
+  apiVersion?: string;
   debug?: boolean;
 }
 
 export class Bitbucket {
   constructor(options?: BitbucketOptions) {
     Object.assign(this.options, options);
-    this.log('Bitbucket Initialized')
+    this.log('Bitbucket Initialized');
   }
 
-  private options: BitbucketOptions;
+  private options: BitbucketOptions = {
+    apiUrl: 'https://api.bitbucket.org',
+    apiVersion: '2.0',
+    debug: false
+  }
 
   private log(...msgs: string[]) {
     if (this.options && this.options.debug) {
